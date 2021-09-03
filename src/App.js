76ab8b3b3cbf45963c98ef2079bd9b18f1e65d2f
Ignacio <ignacio@Ignacios-MacBook-Pro.local>
2021-09-03 15:31:46 -0600

    beginning new react art website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import Items from "./Items";
 
-function App() {
+const itemData = Items;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    // overflow: "hidden",
+  },
+  imageList: {
+    width: "70%",
+    // height: "100vh",
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
+}));
+
+export default function TitlebarImageList() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <ImageList rowHeight={280} cols={4} className={classes.imageList}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img src={item.img} alt={item.title} />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   );
 }
-
-export default App;
---
+++ src/Items.js
@@ -0,0 +1,139 @@
+const itemData = [
+  {
+    img: "images/1.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/2.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/3.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/4.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/5.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/6.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/7.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/8.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/9.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/11.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/12.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/13.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/14.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/15.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/16.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/17.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/18.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/19.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/20.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/21.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/22.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/23.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/24.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/25.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/26.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/27.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+  {
+    img: "images/28.jpg",
+    title: "Sin Titulo",
+    author: "Ignacio Quiros",
+  },
+];
+
+export default itemData;
